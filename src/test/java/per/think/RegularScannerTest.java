214decefc9669c9c70e0a7f44daa7bc7115048ab
elto <404088770@qq.com>
2017-10-28 19:38:19 +0800

    RegularScanner demo

--- src/test/java/per/think/RegularScannerTest.java
+++ src/test/java/per/think/RegularScannerTest.java
@@ -1,7 +1,11 @@
 package per.think;
 
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+
 /**
  * Created by elotoma on 2017/10/28.
+ * 使用正则表达式扫描Scanner
  */
 public class RegularScannerTest {
 }
